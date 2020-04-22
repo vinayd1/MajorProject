b30beea07b4c431f9499056e50728eb6fc92df70
vinayd1 <agrawalv4@gmail.com>
2020-04-23 01:56:31 +0530

    Nav

--- src/components/App.js
+++ src/components/App.js
@@ -90,7 +90,7 @@ class App extends Component {
     return (
       <div>
         <NavBar name="Major Project" account={account} />
-        <div className="container-fluid mt-5">
+        <div className="container mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <button onClick={this.triggerContract}>Click</button>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const NavBar = ({name, account}) => <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-    <p className="navbar-brand col-sm-3 col-md-2 mr-0 mb-0">{name}</p>
-    <p className="text-white mr-3 mb-0">{account}</p>
+const NavBar = ({ name, account }) => <nav className="navbar navbar-dark fixed-top bg-dark d-block p-0 shadow">
+    <div className="d-flex flex-wrap justify-content-between align-items-center px-2 pt-1 pb-2">
+        <p className="text-white mr-0 mb-0 h2">{name}</p>
+        <p className="text-white mr-3 mb-0">{account}</p>
+    </div>
 </nav>
 
 export default NavBar;
